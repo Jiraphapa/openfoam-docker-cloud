c7f6654a3f12eef1dfb00c6b3d57fbab51421fcc
Jiraphapa <jira.jiravaraphan@gmail.com>
2020-07-24 02:57:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ echo "*******************************************************"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 docker run hello-world
