fbe8f2d38bb0a584a01438300f242ddcb549fec3
Jiraphapa <jira.jiravaraphan@gmail.com>
2020-07-24 02:48:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ echo "*******************************************************"
 echo "Installing Docker on your system:"
 echo "*******************************************************"
 
-
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
@@ -18,7 +17,24 @@ echo "*******************************************************"
 docker pull jiraphapa/openfoam-docker-cloud
 
 echo "*******************************************************"
+echo "Below are available docker images on your system"
+echo "*******************************************************"
+
+docker images
+
+echo "*******************************************************"
 echo "Creating and running OpenFOAM container"
 echo "*******************************************************"
 
 docker run --restart=always jiraphapa/openfoam-docker-cloud
+
+echo "*******************************************************"
+echo "Below are running containers on your system"
+echo "*******************************************************"
+
+docker ps
+
+echo "*******************************************************"
+echo "Run the ./testOpenFOAM script to test the container"
+echo "*******************************************************"
+
